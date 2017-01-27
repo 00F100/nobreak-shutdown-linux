46ad2283c400fcbff8e6fa9262e5a0552257af9d
João Moraes <joaomoraesbr@gmail.com>
2017-01-26 22:31:37 -0200

    Update to change language

--- nobreak.sh
+++ nobreak.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
-DATA=`date +%d/%m/%Y`
-HORA=`date +%H:%M:%S`
+DATE=`date +%d/%m/%Y`
+HOUR=`date +%H:%M:%S`
 LOG=/var/log/nobreak.log
 LOGFAIL=/var/log/nobreak-fail.log
+MSG_POWER_OFF="The power is gone away"
+MSG_LINE="-----------------------------------------------------------------------------------------------"
+MSG_PING="$DATE $HOUR FAIL (ping $1)"
+MSG_SLEEP="Sleep $5 minutes"
+MSG_TRY="Try connect host..."
+MSG_POWER_OFF_CONFIRM="The power do not return after $5 minutes, turn off server"
+
 ping -c "$2" "$1" | grep -in "64 bytes" > "$LOG"
 CONTENTLOG="`cat $LOG`"
 touch "$LOGFAIL"
 if [ -z "$CONTENTLOG" ] 
 then
-  echo "----------------" >> "$LOGFAIL"
-  echo "$DATA $HORA FAIL (ping $1)" >> "$LOGFAIL"
+  echo $MSG_POWER_OFF
+  echo $MSG_LINE >> "$LOGFAIL"
+  echo $MSG_POWER_OFF >> "$LOGFAIL"
+  echo $MSG_PING >> "$LOGFAIL"
   ping -c "$3" "$1" >> "$LOGFAIL"
-  echo "A ENERGIA CAIU!"
-  echo "A ENERGIA CAIU!" >> "$LOGFAIL"
-  echo "-----------------------------------------------------------------------------------------------" >> "$LOGFAIL"
-  echo "$DATA $HORA - O SERVIDOR ANALISOU E DETECTOU QUEDA DE ENERGIA" >> "$LOGFAIL"
-  echo "SLEEP DE $5 MINUTOS"
+  echo $MSG_LINE >> "$LOGFAIL"
+  echo $MSG_SLEEP
+  echo $MSG_SLEEP >> "$LOGFAIL"
   sleep $4
-  echo "NOVA TENTATIVA DE CONECTAR AO SERVIDOR"
+  DATE=`date +%d/%m/%Y`
+  HOUR=`date +%H:%M:%S`
+  echo $MSG_TRY
+  echo $MSG_TRY >> "$LOGFAIL"
+  echo $MSG_LINE >> "$LOGFAIL"
+  MSG_PING="$DATE $HOUR FAIL (ping $1)"
   ping -c "$2" "$1" | grep -in "64 bytes" > "$LOG"
   CONTENTLOGT="`cat $LOG`"
   if [ -z "$CONTENTLOGT" ]
   then
-    echo "A LUZ NAO RETORNOU EM $5 MINUTOS, DESLIGANDO SERVIDOR"
-    echo "A LUZ NAO RETORNOU EM $5 MINUTOS, DESLIGANDO SERVIDOR" >> "$LOGFAIL"
-    poweroff
+    ping -c "$3" "$1" >> "$LOGFAIL"
+    echo $MSG_LINE >> "$LOGFAIL"
+    echo $MSG_POWER_OFF_CONFIRM
+    echo $MSG_POWER_OFF_CONFIRM >> "$LOGFAIL"
+    # shutdown -r 1 "The server will reboot in 1 minute"
   fi
 fi
